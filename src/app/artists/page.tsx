143e895a9ea04a519e4bb391bcc1d63ebc96ab9c
mkiran <152247979+M6839@users.noreply.github.com>
2025-06-24 19:48:54 +0530

    solved useSearchParams error

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -1,44 +1,16 @@
+import { Suspense } from 'react';
+import ArtistListingPage from '@/components/ArtistListingPage'
+import React from 'react'
 
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import ArtistCard from '@/components/ArtistCard';
-import FilterBlock from '@/components/FilterBlock';
-import artistsData from '@/data/artistsdata.json';
-
-export default function ArtistListingPage() {
-  const searchParams = useSearchParams();
-  const defaultCategory = searchParams.get('category') || '';
-  
-  const [category, setCategory] = useState(defaultCategory);
-  const [filtered, setFiltered] = useState(artistsData);
-  const [price,setPrice]=useState('');
-
-  useEffect(() => {
-    if (!category && !price) {
-      setFiltered(artistsData);
-    }
-    else if(category && !price){
-      setFiltered(artistsData.filter((a) => a.category === category ));
-    }
-    else if(!category && price){
-      setFiltered(artistsData.filter((a) => a.priceRange===price));
-    }
-    else {
-      setFiltered(artistsData.filter((a) => a.category === category && a.priceRange===price));
-    }
-  }, [category,price]);
-
+const page = () => {
   return (
     <div className="p-6 min-h-screen bg-white text-black">
       <h1 className="text-2xl font-bold mb-4">Explore Artists</h1>
-      <FilterBlock category={category} setCategory={setCategory} price={price} setPrice={setPrice} />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
-        {filtered.map((artist) => (
-          <ArtistCard key={artist.id} {...artist} />
-        ))}
-      </div>
+      <Suspense fallback={<p>Loading...</p>}>
+      <ArtistListingPage/>
+      </Suspense>
     </div>
-  );
+  )
 }
+
+export default page
---
+++ src/components/ArtistListingPage.tsx
@@ -0,0 +1,42 @@
+
+'use client';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import ArtistCard from '@/components/ArtistCard';
+import FilterBlock from '@/components/FilterBlock';
+import artistsData from '@/data/artistsdata.json';
+
+export default function ArtistListingPage() {
+  const searchParams = useSearchParams();
+  const defaultCategory = searchParams.get('category') || '';
+  
+  const [category, setCategory] = useState(defaultCategory);
+  const [filtered, setFiltered] = useState(artistsData);
+  const [price,setPrice]=useState('');
+
+  useEffect(() => {
+    if (!category && !price) {
+      setFiltered(artistsData);
+    }
+    else if(category && !price){
+      setFiltered(artistsData.filter((a) => a.category === category ));
+    }
+    else if(!category && price){
+      setFiltered(artistsData.filter((a) => a.priceRange===price));
+    }
+    else {
+      setFiltered(artistsData.filter((a) => a.category === category && a.priceRange===price));
+    }
+  }, [category,price]);
+
+  return (
+    <div className="p-6 min-h-screen bg-white text-black">
+      <FilterBlock category={category} setCategory={setCategory} price={price} setPrice={setPrice} />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+        {filtered.map((artist) => (
+          <ArtistCard key={artist.id} {...artist} />
+        ))}
+      </div>
+    </div>
+  );
+}
